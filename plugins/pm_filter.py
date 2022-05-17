d0b0fff6e0813c7924db8ae3ffb0afdda3a99a79
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-17 22:07:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -99,7 +99,6 @@ async def next_page(bot, query):
         btn.append(
             [InlineKeyboardButton(f"🗓 {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
-        )
     else:
         btn.append(
             [
