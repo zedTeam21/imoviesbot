a9d36dc8945cf50fc856f595d0699b1c9d8c544a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-14 07:10:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -116,6 +116,24 @@ Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provid
 
 ©️Mantained Bʏ  @sahid_malik</b>"""
 
+    GHHM_TXT = """<b>7k User 💖 Thanks For Your Support...
+
+𝖩𝗎𝗌𝗍 𝖠𝖽𝖽 𝖮𝗎𝗋 𝖡𝗈𝗍 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 𝖠𝗌 𝖠𝖽𝗆𝗂𝗇, 𝖨𝗍 𝖶𝗂𝗅𝗅 𝖯𝗋𝗈𝗏𝗂𝖽𝖾 𝖬𝗈𝗏𝗂𝖾𝗌 𝖳𝗁𝖾𝗋𝖾... 😎
+
+
+     ♋️ 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀 ♋️
+
+✪ AutoFilter, Manual Filter
+✪ IMDb HD Posters
+✪ IMDb Real Details
+✪ Two Buttons Mode
+✪ Force Subscribe
+✪ Extra Features: download songs, download you tube video, URL Shortner,  
+
+⚙ More Features Adding Soon</b> 😎"""
+
+    GHHN_TXT = """Extra features"""
+
     MALIK_TXT =  """<b>Donation</b>
    
    <b>Developer is Super Noob..  Just Learning from Official Docs..  Please Donate the developer for Keeping the Service Alive...</b>
