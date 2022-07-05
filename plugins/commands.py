39c586548685c670fc94a021254571c809a657d9
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-05 08:22:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,8 +33,8 @@ async def start(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-        photo=(PHTT),
+        await message.reply_video(
+        video=(PHTT),
         caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME)),
         reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
