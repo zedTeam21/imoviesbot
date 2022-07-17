5885f8d2aa2d9de7ffea4180bd73bef9c82dc90b
test00123 <109013696+test00123@users.noreply.github.com>
2022-07-17 13:31:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,23 +416,20 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('✳️ Add Me To Your Groups ✳️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('ADD ME TO THE GROUP', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔥 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 🔥', url='https://t.me/+ZeZNvt43B4o3ZmJl'),
-            InlineKeyboardButton('❗️Bot Owner❗️', callback_data='owner'),
-            InlineKeyboardButton('⚡️ UPDATES ⚡️', url='https://t.me/m_house786')
+            InlineKeyboardButton('CHANNELS', url='https://t.me/cinemica'),
+            InlineKeyboardButton('GROUP', url='https://t.me/+Spzsxjag4AEwN2U1')
             ],[
-            InlineKeyboardButton('♻️ Help ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ About ♻️', callback_data='about')
+            InlineKeyboardButton(' HELP ', callback_data='help'),
+            InlineKeyboardButton('REQUEST', url='https://t.me/cinemica_bot')
             ],[
-            InlineKeyboardButton('Search inline', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('❤️ Donation ❤️', callback_data='dinette')
+            InlineKeyboardButton('Search INLINE', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('❤️ DONATION ❤️', callback_data='dinette')
             ],[
-            InlineKeyboardButton('🚀 Download YouTube video 🛰', callback_data='videos')
+            InlineKeyboardButton('🔗 URL SHORTER 🔗', callback_data='urlshortn')
             ],[
-            InlineKeyboardButton('🔗 Url Shortner 🔗', callback_data='urlshortn')
-            ],[
-            InlineKeyboardButton('✅ Subscribe my YouTube channel ✅', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A')
+            InlineKeyboardButton('SUBSCRIBE MY YT CHANNELS ', url='https://www.youtube.com/channel/UC2_U9WYQaopjXGZ7n9HbN3Q')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
