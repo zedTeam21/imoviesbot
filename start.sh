92bdaa7282e71353d51980be3d15d9763c135343
test00123 <109013696+test00123@users.noreply.github.com>
2022-07-17 07:38:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/file-filter-bot.git /file-filter-bot
+  git clone https://github.com/test00123/new-repo.git /new-repo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /file-filter-bot
+  git clone $UPSTREAM_REPO /new-repo
 fi
-cd /file-filter-bot
+cd /new-repo
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
