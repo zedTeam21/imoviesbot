b56e1c788f66f5835442d8a0422af1e23f936c10
Techwrdd <107527532+Techwrdd@users.noreply.github.com>
2022-07-17 15:52:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/test00123/new-repo.git /new-repo
+  git clone https://github.com/Techwrdd/cinemicabot.git /cinemicabot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /new-repo
+  git clone $UPSTREAM_REPO /cinemicabot
 fi
-cd /new-repo
+cd /cinemicabot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
