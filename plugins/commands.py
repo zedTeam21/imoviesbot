00928a3db77b5116240ed8568ede37eddd8a0f6a
test00123 <109013696+test00123@users.noreply.github.com>
2022-07-17 08:58:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,15 +26,15 @@ async def start(client, message):
                 InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],
             [
-                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/m_house786')
+                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/cinemica')
             ],
             [
                 InlineKeyboardButton('❇️ Help ❇️', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_video(
-        video=(PHTT),
+        await message.reply_photo(
+        photo=(PHTT),
         caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME)),
         reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
