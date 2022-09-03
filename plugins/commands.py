fa0bcf140ca14b524132c14d6b921b3e6a283845
Techwrdd <107527532+Techwrdd@users.noreply.github.com>
2022-09-03 09:55:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -257,7 +257,7 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-    await client.send_cached_media(
+    dl = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
@@ -270,7 +270,8 @@ async def start(client, message):
             ]
         )
     )                
-
+    await asyncio.sleep(300)
+    await dl.delete()
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
            
