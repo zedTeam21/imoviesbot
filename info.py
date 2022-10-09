38aa4d876d07c8fff5f2db8e1fc3fcd49a77d2b3
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2022-10-09 19:33:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,10 +22,10 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
 MALIK_PH = environ.get("MALIK_PH", "https://telegra.ph/file/befa0290a2eb14dfe0abe.png")
-SMART_PIC = environ.get("SMART_PIC", "https://telegra.ph/file/6d6b68a763832142fd9f2.jpg")
+SMART_PIC = environ.get("SMART_PIC", "https://telegra.ph/file/9a86852df9e70951217cd.png")
 VIDEO_VD = environ.get("VIDEO_VD", "https://telegra.ph/file/566ff238e36d9f2425568.mp4")
 PHT = environ.get("PHT", "https://telegra.ph/file/aa0de604ec7834e3d7b51.jpg")
-PHTT = environ.get("PHTT", "https://telegra.ph/file/91b62afcb304705103e64.jpg")
+PHTT = environ.get("PHTT", "https://telegra.ph/file/471b95f6d95471b115c4a.png")
 M_NT_F = environ.get("M_NT_F", "https://telegra.ph/file/b9c8a8240590623ba43ee.jpg")
 
 # Admins, Channels & Users
