2ed31332d02040997b8300540733a86aaedb76aa
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2022-10-09 13:49:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /imoviesbot
 fi
-cd /cinemicabot
+cd /imoviesbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
