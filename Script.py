13fa0acbd9e8f4737bb7c96cea92f1fa90b051a6
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2022-10-09 19:15:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -21,7 +21,7 @@ HERE IS THE HELP FOR MY COMMANDS ."""
 - Filter is the feature were users can set automated replies for a particular keyword and cinemica will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. cinemica have admin privillage.
+1. Sever_z have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
